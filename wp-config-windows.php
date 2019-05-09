68cb04ce838c79cfde459520956a88171810290d
RobertSzacsko <robiiszacsko@gmail.com>
2019-05-09 10:13:02 +0300

    refactoring wp configs

--- wp-config-windows.php
+++ wp-config-windows.php
@@ -78,12 +78,3 @@ $table_prefix  = 'vg_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', true);
-
-/* That's all, stop editing! Happy blogging. */
-
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
-
-/** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
--- wp-config.php
+++ wp-config.php
@@ -5,3 +5,12 @@ if ($_SERVER['DESKTOP_SESSION'] === 'ubuntu' || strpos($_SERVER['SERVER_SOFTWARE
 } else {
 	require_once(ABSPATH . 'wp-config-windows.php');
 }
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');

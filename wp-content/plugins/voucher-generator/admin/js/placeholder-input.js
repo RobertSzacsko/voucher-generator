6d836703aa1fde195902935607cd6b7acaf4fb62
RobertSzacsko <robiiszacsko@gmail.com>
2019-05-14 14:12:44 +0300

    add placeholder for input

---
+++ wp-content/plugins/voucher-generator/admin/js/placeholder-input.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+    $('.vg-placeholder-span').on('click', function () {
+        $(this).prev('.vg-placeholder-input').focus();
+    });
+
+    $('.vg-placeholder-input').on('input',function(e){
+        (function ($element) {
+            if($element.val() !== '') {
+                $element.addClass('vg-placeholder-input-focus');
+            } else {
+                $element.removeClass('vg-placeholder-input-focus');
+            }
+        }($('.vg-placeholder-input')));
+    }); 
+});

eaecf2723d66d6c703aac9d9a150b0550218fd53
RobertSzacsko <robiiszacsko@gmail.com>
2019-07-04 10:06:12 +0300

    small bug fixed

--- wp-config.php
+++ wp-config.php
@@ -1,9 +1,15 @@
 <?php
 
+
 if ($_SERVER['DESKTOP_SESSION'] === 'ubuntu' || strpos($_SERVER['SERVER_SOFTWARE'], 'Ubuntu') !== false) {
 	require_once(ABSPATH . 'wp-config-linux.php');
 } else {
 	require_once(ABSPATH . 'wp-config-windows.php');
+}
+
+if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+	$_SERVER['HTTPS']='on';//
+	$_SERVER['SERVER_PORT'] = 443;
 }
 
 /* That's all, stop editing! Happy blogging. */
